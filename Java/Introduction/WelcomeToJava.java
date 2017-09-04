5dc37abd34437521fcdaae200cf59a3d3b45e5f7
Sapayth Hossain <sapayth@users.noreply.github.com>
2017-09-04 13:52:08 +0600

    Update WelcomeToJava.java

--- Java/Introduction/WelcomeToJava.java
+++ Java/Introduction/WelcomeToJava.java
@@ -10,7 +10,7 @@ public class WelcomeToJava {
 
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-        System.out.print("Hello, World");
-        System.out.print("Hello, Java");
+        System.out.print("Hello, World.");
+        System.out.print("Hello, Java.");
     }
 }
